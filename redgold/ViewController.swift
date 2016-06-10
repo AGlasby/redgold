ec7d1a4367ef0879e0e739ef093dbd54b168cfc0
Alan Glasby <alan@glasby.me.uk>
2016-06-10 15:48:20 +0100

    Initial Commit

---
+++ redgold/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  redgold
+//
+//  Created by Alan Glasby on 10/06/2016.
+//  Copyright © 2016 Alan Glasby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
