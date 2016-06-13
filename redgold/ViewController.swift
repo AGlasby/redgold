0f5ed7fc9b82cca519edd6324d15a9e4d42225e0
Alan Glasby <alan@glasby.me.uk>
2016-06-13 09:17:51 +0100

    initial commit

--- redgold/ViewController.swift
+++ redgold/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redImg: UIImageView!
+    @IBOutlet weak var goldImg: UIImageView!
+    
+    @IBOutlet weak var hideRed: UIButton!
+    @IBOutlet weak var hideGold: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeRedHide(sender: AnyObject) {
+        redImg.hidden = true
+    }
 
+    @IBAction func makeGoldHide(sender: AnyObject) {
+        goldImg.hidden = true
+    }
 }
 
